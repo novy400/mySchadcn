fcf18b278a8c426e5460c35eed423086c24ea00e
yvon.vieville <yvon.vieville@gmail.com>
2026-04-22 13:47:44 +0200

    feat: Update CustomerList component to replace header with label for DataTable columns

--- src/modules/crm/customers/CustomerList.tsx
+++ src/modules/crm/customers/CustomerList.tsx
@@ -5,13 +5,13 @@ import { TextField } from '@/components/admin/text-field';
 export const CustomerList = () => (
   <List>
     <DataTable rowClick="show">
-      <DataTable.Col source="id" header="ID">
+      <DataTable.Col source="id">
         <TextField source="id" />
       </DataTable.Col>
-      <DataTable.Col source="name" header="Raison Sociale">
+      <DataTable.Col source="name" label="Raison Sociale">
         <TextField source="name" />
       </DataTable.Col>
-      <DataTable.Col source="type" header="Forme Juridique">
+      <DataTable.Col source="type" label="Forme Juridique">
         <TextField source="type" />
       </DataTable.Col>
     </DataTable>
