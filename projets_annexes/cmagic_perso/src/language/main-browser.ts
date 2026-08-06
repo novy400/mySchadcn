57e1bf9af571b7e658e31dd559177158d4fb3426
Yvon Vieville <novy400@gmail.com>
2026-08-06 17:13:54 +0200

    chore(cmagic): migrate Langium engine to 4.3.1

--- projets_annexes/cmagic_perso/src/language/main-browser.ts
+++ projets_annexes/cmagic_perso/src/language/main-browser.ts
@@ -1,6 +1,6 @@
 import { EmptyFileSystem } from 'langium';
 import { startLanguageServer } from 'langium/lsp';
-import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser';
 import { createCmagicServices } from './cmagic-module.js';
 
 declare const self: DedicatedWorkerGlobalScope;
--- projets_annexes/cmagic_perso/test/validating/validating.test.ts
+++ projets_annexes/cmagic_perso/test/validating/validating.test.ts
@@ -59,7 +59,7 @@ function checkDocumentValid(document: LangiumDocument): string | undefined {
           ${document.parseResult.parserErrors.map(e => e.message).join('\n  ')}
     `
         || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
-        || !isModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a '${Model}'.`
+        || !isModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a '${Model.$type}'.`
         || undefined;
 }
 
